db3e2843511596cd9b21a82b968669aaa02ff4f2
Matheus Cavalcante <mscavalcante2024@gmail.com>
2024-07-15 16:39:57 -0300

    Added the default color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        branco: '#FFFFFF',
+        preto: '#000000',
+        pretoClaro: '#33312D',
+        cinza: '#D9D9D9',
+        cinzaClaro: '#EEEEEE',
+        cinzaEscuro: '#575757',
+        douradoLogo: '#FEC92B',
+        douradoClaro: '#FABD14',
+        douradoEscuro: '#CFA633',
+        douradoEscuroFraco: '#7A6D47',
+        verde: '#28C900',
+        vermelho: '#C90000',
+      }
+    },
   },
   plugins: [],
 }
